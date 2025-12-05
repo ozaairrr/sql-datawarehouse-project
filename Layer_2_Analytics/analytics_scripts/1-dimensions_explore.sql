18ae1c7c4a05ca275e9dc16f6ce0041757454747
Shaikh Mohammad Ozair <133234308+ozaairrr@users.noreply.github.com>
2025-12-05 18:46:48 +0530

    Update 1-dimensions_explore.sql

--- Layer_2_Analytics/analytics_scripts/1-dimensions_explore.sql
+++ Layer_2_Analytics/analytics_scripts/1-dimensions_explore.sql
@@ -1,1 +1,24 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold_dim_customers
+ORDER BY country;
 
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    sub_category, 
+    product_name 
+FROM gold_dim_products
+ORDER BY category, sub_category, product_name;
